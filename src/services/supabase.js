4b3a6d203d3d8cca97942688c4b76224ad02732b
Ritmeester-Coder <ritmeester@atomicmail.io>
2026-09-10 12:30:15 +0200

    fix: Updated Supabase url in .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = "https://dvzmtobhlgryvcocvgda.supabase.co";
 
-const supabasePublishableKey =
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabasePublishableKey = "sb_publishable_CjIFdPbMKHg_Pep7C2SShA_Yw3sR5PP"
 
 export const supabase = createClient(
   supabaseUrl,
